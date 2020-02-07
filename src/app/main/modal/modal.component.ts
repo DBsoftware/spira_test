7934b96df2e6c443433615cd40463a950d162f2b
DBsoftware <blueashym@gmail.com>
2020-02-07 08:10:02 -0500

    cambio de modalidad de editar preguntas

--- src/app/main/modal/modal.component.ts
+++ src/app/main/modal/modal.component.ts
@@ -15,33 +15,47 @@ export class ModalComponent implements OnInit {
   dataApi
   data = dat;
   labels = ['Uno', 'Dos', 'Tres'];
+  preguntas;
+  PreguntaSeleccionada;
   constructor(
     public dialogRef: MatDialogRef<ModalComponent>,
     @Inject(MAT_DIALOG_DATA) public dataExterna: any) {}
 
   ngOnInit() {
     this.formulario = new FormGroup({})
+    this.formulario.addControl('pregunta', new FormControl(null, Validators.required))
 
     this.dataApi = of({datos: this.data})
     .pipe(
       map(e => e['datos']),
       map((e: any[]) => e.sort(({orden:a},{orden: b}) => a -b)),
+      tap(e => this.preguntas = e),
       map((e: any[]) => e.map((a,i)=>
-       ({...a, label: `pregunta ${this.labels[i]}`, name: `pregunta_${i+1}` }))),
-      tap(e => {
-        e.forEach((a,i) => {
-          this.formulario.addControl(a.name, new FormControl(a.pregunta, Validators.required))
-        })
-      })
+       ({...a, label: `pregunta ${this.labels[i]}` }))),
      )
 
   }
+
+  foods: {value:string, viewValue: string}[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+
   onNoClick(): void {
     this.dialogRef.close();
   }
 
   guardar(){
     console.log('gurdando')
+  }
+
+  seleccionPregunta(e){
+    this.PreguntaSeleccionada = this.preguntas[e.value - 1]
+    this.formulario.get('pregunta').setValue(this.PreguntaSeleccionada.pregunta)
+    console.log(this.PreguntaSeleccionada)
+
   }
 
 }
